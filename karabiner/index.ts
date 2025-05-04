7dcf392dd4966271e3ef2bdfd961e0e92e4fb88e
tadashi-aikawa <syou.maman@gmail.com>
2025-05-04 14:00:53 +0900

    feat(karabiner): NORMALモードを追加

--- karabiner/index.ts
+++ karabiner/index.ts
@@ -2,12 +2,23 @@ import {
   layer,
   map,
   rule,
+  ToEvent,
   toKey,
+  toRemoveNotificationMessage,
+  toSetVar,
   writeToProfile,
 } from "https://deno.land/x/karabinerts@1.31.0/deno.ts";
 
+type Mode = "NORMAL";
+const withTerminateMode = (mode: Mode, toEvent: ToEvent) => [
+  toEvent,
+  toSetVar(mode, 0),
+  toSetVar("__layer", 0),
+  toRemoveNotificationMessage(`layer-${mode}`),
+];
+
 writeToProfile("Default profile", [
-  layer(";").leaderMode().notification().manipulators({
+  layer(";").leaderMode().manipulators({
     a: toKey("equal_sign"), // ^
     d: toKey("3", "shift"), // #
     e: [toKey("spacebar"), toKey("hyphen", "shift"), toKey("spacebar")], // <space>=<space>
@@ -39,8 +50,29 @@ writeToProfile("Default profile", [
       toKey("left_arrow"),
     ], // {}
     ";": toKey("semicolon"),
-    "escape": toKey("japanese_kana"),
   }),
+
+  // NORMALモード
+  layer("grave_accent_and_tilde", "NORMAL").leaderMode({
+    sticky: true,
+    escape: ["grave_accent_and_tilde", "escape"],
+  })
+    .notification(
+      "NORMAL",
+    ).manipulators([
+      map("j").to("down_arrow"),
+      map("k").to("up_arrow"),
+      map("h").to("left_arrow"),
+      map("l").to("right_arrow"),
+      map("a").to("home"),
+      map(";").to("end"),
+      map("f").to("home", "control"),
+      map("f", "shift").to("end", "control"),
+      map("u").to("delete_or_backspace"),
+      map("o").to("delete_or_backspace", "fn"),
+
+      map("quote").to(withTerminateMode("NORMAL", toKey("japanese_kana"))),
+    ]),
   rule("ESC").manipulators([
     map("escape").to([toKey("escape"), toKey("japanese_eisuu")]),
   ]),
