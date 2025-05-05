7c4a2e3f4f456060bb927f91965332d9c0fa9a9c
tadashi-aikawa <syou.maman@gmail.com>
2025-05-05 10:44:47 +0900

    refactor(karabiner): リファクタリング

--- karabiner/index.ts
+++ karabiner/index.ts
@@ -1,16 +1,15 @@
 import {
   ifApp,
   layer,
-  map,
   ModifierParam,
   rule,
-  ToEvent,
   toKey,
   ToKeyParam,
   toNotificationMessage,
   toPaste,
   toRemoveNotificationMessage,
   toSetVar,
+  withCondition,
   withModifier,
   writeToProfile,
 } from "https://deno.land/x/karabinerts@1.31.0/deno.ts";
@@ -22,10 +21,6 @@ const terminateMode = (mode: Mode) => [
   toSetVar(mode, 0),
   toSetVar("__layer", 0),
   toRemoveNotificationMessage(MODE_NOTIFICATION_ID),
-];
-const withTerminateMode = (mode: Mode, toEvent: ToEvent) => [
-  toEvent,
-  ...terminateMode(mode),
 ];
 const startMode = (mode: Mode) => [
   toSetVar(mode, 1),
@@ -48,53 +43,74 @@ const App = {
   not: (appName: AppName) => ifApp(appIdentifierMapper[appName]).unless(),
 };
 
+/**
+ * 単一のキーで成立するJISキーマッピング
+ */
+const UJM = {
+  "^": "equal_sign",
+  " ": "spacebar",
+  ":": "quote",
+  ";": "semicolon",
+  "/": "keypad_slash",
+  "<-": "left_arrow",
+  "->": "right_arrow",
+  "down": "down_arrow",
+  "up": "up_arrow",
+  "bs": "delete_or_backspace",
+  "home": "home",
+  "end": "end",
+  "ESC": "escape",
+  "enter": "return_or_enter",
+  "tab": "tab",
+  "半全": "grave_accent_and_tilde",
+  "かな": "japanese_kana",
+  "英数": "japanese_eisuu",
+} as const satisfies Record<string, ToKeyParam>;
+
+function toSingleTupleMap<T extends Record<string, string>>(map: T) {
+  const out = {} as { [K in keyof T]: [T[K]] };
+  for (const k in map) {
+    out[k] = [map[k]] as const;
+  }
+  return out;
+}
+
+/**
+ * JISキーマッピング
+ */
 const JM = {
   "{": ["close_bracket", "shift"],
   "}": ["backslash", "shift"],
-  "^": ["equal_sign"],
-  "#": ["3", "shift"],
-  "$": ["4", "shift"],
   "~": ["equal_sign", "shift"],
   "_": ["international1", "shift"],
   "|": ["international3", "shift"],
   "`": ["open_bracket", "shift"],
   "!": ["1", "shift"],
+  '"': ["2", "shift"],
+  "#": ["3", "shift"],
+  "$": ["4", "shift"],
   "%": ["5", "shift"],
   "&": ["6", "shift"],
-  '"': ["2", "shift"],
   "'": ["7", "shift"],
   "(": ["8", "shift"],
   ")": ["9", "shift"],
   "=": ["hyphen", "shift"],
-  " ": ["spacebar"],
-  ":": ["quote"],
-  ";": ["semicolon"],
-  "/": ["keypad_slash"],
-  "<-": ["left_arrow"],
-  "->": ["right_arrow"],
-  "down": ["down_arrow"],
-  "up": ["up_arrow"],
   "del": ["delete_or_backspace", "fn"],
-  "bs": ["delete_or_backspace"],
-  "無変換": ["grave_accent_and_tilde"],
-  "home": ["home"],
+  ...toSingleTupleMap(UJM),
 } as const satisfies Record<string, [ToKeyParam, ModifierParam?]>;
 
 // as [any] はdeno lint error回避
 type JKey = keyof typeof JM;
-const toJKey = (key: JKey) => toKey(...JM[key] as [any]);
-
 // as [any] はdeno lint error回避
-type UniJMKey = {
-  [K in JKey]: (typeof JM)[K] extends { length: 1 } ? K : never;
-}[JKey];
+type UniJMKey = keyof typeof UJM;
+
+const toJKey = (key: JKey) => toKey(...JM[key] as [any]);
 const toJKeyWith = (key: UniJMKey, modifier: ModifierParam, repeat?: number) =>
   repeat
     ? [...Array(repeat).keys()].map(
       (_) => toKey(...JM[key] as [any], modifier),
     )
     : toKey(...JM[key] as [any], modifier);
-
 const toJKeys = (...args: JKey[] | [JKey, repeat: number]) => {
   if (typeof args[1] === "number") {
     return [...Array(args[1]).keys()].map(
@@ -106,201 +122,199 @@ const toJKeys = (...args: JKey[] | [JKey, repeat: number]) => {
   return keys.map(toJKey);
 };
 
+const UNUSED_KEY = "caps_lock";
+
 /**
  * 基本的に先に定義した方が採用されるので注意
  * 条件付きやmodeの設定は先に記載すること
  */
 writeToProfile("Default profile", [
-  // NORMALモード (caps_lockは使わないと判断した捨てキー)
-  layer("caps_lock", "NORMAL").leaderMode({
+  layer(UNUSED_KEY, "NORMAL").leaderMode({
     sticky: true,
   })
     .manipulators([
+      withCondition(App.is("Ghostty"))([
+        {
+          ";": toKey("end"),
+          a: toKey("home"),
+          f: toJKeyWith("home", "control"),
+        },
+        withModifier("shift")({
+          f: toJKeyWith("end", "control"),
+        }),
+      ]),
+
       {
+        ",": [...terminateMode("NORMAL"), toKey("japanese_eisuu")],
+        ".": [toJKey("="), ...terminateMode("NORMAL")],
+        ";": toJKeyWith("->", "command"),
+        [UJM.ESC]: [...terminateMode("NORMAL"), toKey("japanese_eisuu")],
+        [UJM.半全]: [...terminateMode("NORMAL"), toKey("japanese_eisuu")],
+        [UJM[":"]]: [...terminateMode("NORMAL"), toKey("japanese_kana")],
+        a: toJKeyWith("<-", "command"),
+        c: toKey("c", "command"),
+        d: toJKey("bs"),
+        e: toJKeyWith("<-", "command"),
+        f: toJKeyWith("up", "command"),
+        g: changeMode("NORMAL", "RANGE"),
+        h: toJKey("<-"),
         j: toJKey("down"),
         k: toJKey("up"),
         l: toJKey("->"),
-        h: toJKey("<-"),
-        u: toJKey("bs"),
-        d: toJKey("bs"),
+        m: [...terminateMode("NORMAL"), toKey("japanese_eisuu")],
         o: toJKey("del"),
-        c: toKey("c", "command"),
-        v: toKey("v", "command"),
         q: toKey("w", "command"),
-        e: toJKeyWith("<-", "command"),
-        z: toKey("z", "command"),
+        r: changeMode("NORMAL", "SPECIAL"),
+        u: toJKey("bs"),
+        v: toKey("v", "command"),
         y: toKey("y", "command"),
-        ".": [toJKey("="), ...terminateMode("NORMAL")],
+        z: toKey("z", "command"),
       },
-      // Shift
-      withModifier("shift")({
-        "j": toJKeys("down", 25),
-        "k": toJKeys("up", 25),
-        "l": toJKeys("->", 10),
-        "h": toJKeys("<-", 10),
-        "u": toJKeys("bs", 5),
-        "o": toJKeys("del", 5),
-      }),
-      // Control
-      withModifier("control")({
-        "j": toJKeys("down", 5),
-        "k": toJKeys("up", 5),
-        "l": toJKeys("->", 5),
-        "h": toJKeys("<-", 5),
-        "e": toJKeyWith("->", "command"),
-      }),
-      // 特殊
-      map("a").to("home").condition(App.is("Ghostty")),
-      map("a").to("←", "command").condition(App.not("Ghostty")),
-      map(";").to("end").condition(App.is("Ghostty")),
-      map(";").to("→", "command").condition(App.not("Ghostty")),
-      map("f").to("home", "control").condition(App.is("Ghostty")),
-      map("f").to("↑", "command").condition(App.not("Ghostty")),
-      map("f", "shift").to("end", "control").condition(App.is("Ghostty")),
-      map("f", "shift").to("↓", "command").condition(App.not("Ghostty")),
 
-      map(...JM[":"]).to(withTerminateMode("NORMAL", toKey("japanese_kana"))),
-      map(",").to(withTerminateMode("NORMAL", toKey("japanese_eisuu"))),
-      map("m").to(withTerminateMode("NORMAL", toKey("japanese_eisuu"))),
-      map("escape").to(withTerminateMode("NORMAL", toKey("japanese_eisuu"))),
-      map(...JM.無変換).to(
-        withTerminateMode("NORMAL", toKey("japanese_eisuu")),
-      ),
-      // モード切り替え
-      map("r").to(changeMode("NORMAL", "SPECIAL")),
-      map("g").to(changeMode("NORMAL", "RANGE")),
+      withModifier("shift")({
+        f: toJKeyWith("down", "command"),
+        h: toJKeys("<-", 10),
+        j: toJKeys("down", 25),
+        k: toJKeys("up", 25),
+        l: toJKeys("->", 10),
+        o: toJKeys("del", 5),
+        u: toJKeys("bs", 5),
+      }),
+      withModifier("control")({
+        e: toJKeyWith("->", "command"),
+        h: toJKeys("<-", 5),
+        j: toJKeys("down", 5),
+        k: toJKeys("up", 5),
+        l: toJKeys("->", 5),
+      }),
     ]),
 
-  // RANGEモード (caps_lockは使わないと判断した捨てキー)
-  layer("caps_lock", "RANGE").leaderMode({
+  layer(UNUSED_KEY, "RANGE").leaderMode({
     sticky: true,
   })
     .manipulators([
+      withCondition(App.is("Ghostty"))([
+        {
+          ";": toJKeyWith("end", "shift"),
+          a: toJKeyWith("home", "shift"),
+          f: toJKeyWith("home", ["control", "shift"]),
+        },
+        withModifier("shift")({
+          f: toJKeyWith("end", ["control", "shift"]),
+        }),
+      ]),
+
       {
+        ";": toJKeyWith("->", ["command", "shift"]),
+        [UJM.ESC]: [...terminateMode("RANGE"), toKey("japanese_eisuu")],
+        [UJM.半全]: [...terminateMode("RANGE"), toKey("japanese_eisuu")],
+        a: toJKeyWith("<-", ["command", "shift"]),
+        c: [...changeMode("RANGE", "NORMAL"), toKey("c", "command")],
+        d: [...changeMode("RANGE", "NORMAL"), toJKey("bs")],
+        f: toJKeyWith("up", ["command", "shift"]),
+        g: changeMode("RANGE", "NORMAL"),
+        h: toJKeyWith("<-", "shift"),
         j: toJKeyWith("down", "shift"),
         k: toJKeyWith("up", "shift"),
         l: toJKeyWith("->", "shift"),
-        h: toJKeyWith("<-", "shift"),
-        c: [...changeMode("RANGE", "NORMAL"), toKey("c", "command")],
-        x: [...changeMode("RANGE", "NORMAL"), toKey("x", "command")],
-        u: [...changeMode("RANGE", "NORMAL"), toJKey("bs")],
-        d: [...changeMode("RANGE", "NORMAL"), toJKey("bs")],
         o: [...changeMode("RANGE", "NORMAL"), toJKey("del")],
+        u: [...changeMode("RANGE", "NORMAL"), toJKey("bs")],
+        x: [...changeMode("RANGE", "NORMAL"), toKey("x", "command")],
       },
-      // Shift
-      withModifier("shift")({
-        "j": toJKeyWith("down", "shift", 25),
-        "k": toJKeyWith("up", "shift", 25),
-        "l": toJKeyWith("->", "shift", 25),
-        "h": toJKeyWith("<-", "shift", 25),
-      }),
-      // Control
-      withModifier("control")({
-        "j": toJKeyWith("down", "shift", 5),
-        "k": toJKeyWith("up", "shift", 5),
-        "l": toJKeyWith("->", "shift", 5),
-        "h": toJKeyWith("<-", "shift", 5),
-      }),
-      // 特殊
-      map("a").to("home", "shift").condition(App.is("Ghostty")),
-      map("a").to("←", ["command", "shift"]).condition(App.not("Ghostty")),
-      map(";").to("end", "shift").condition(App.is("Ghostty")),
-      map(";").to("→", ["command", "shift"]).condition(App.not("Ghostty")),
 
-      // 特殊
-      map("g").to(withTerminateMode("SPECIAL", toKey("g", "shift"))),
-      map("f").to("home", ["control", "shift"]).condition(App.is("Ghostty")),
-      map("f").to("↑", ["command", "shift"]).condition(App.not("Ghostty")),
-      map("f", "shift").to("end", ["control", "shift"]).condition(
-        App.is("Ghostty"),
-      ),
-      map("f", "shift").to("↓", ["command", "shift"]).condition(
-        App.not("Ghostty"),
-      ),
-      // モード切り替え
-      map(...JM.無変換).to(
-        withTerminateMode("RANGE", toKey("japanese_eisuu")),
-      ),
-      map("escape").to(withTerminateMode("RANGE", toKey("japanese_eisuu"))),
+      withModifier("shift")({
+        h: toJKeyWith("<-", "shift", 25),
+        j: toJKeyWith("down", "shift", 25),
+        k: toJKeyWith("up", "shift", 25),
+        l: toJKeyWith("->", "shift", 25),
+        f: toJKeyWith("down", ["command", "shift"]),
+      }),
+
+      withModifier("control")({
+        h: toJKeyWith("<-", "shift", 5),
+        j: toJKeyWith("down", "shift", 5),
+        k: toJKeyWith("up", "shift", 5),
+        l: toJKeyWith("->", "shift", 5),
+      }),
     ]),
 
-  // SPECIALモード (caps_lockは使わないと判断した捨てキー)
-  layer("caps_lock", "SPECIAL").leaderMode({
+  layer(UNUSED_KEY, "SPECIAL").leaderMode({
     sticky: true,
   })
     .manipulators([
       {
-        "m": toKey("keypad_1"),
         ",": toKey("keypad_2"),
         ".": toKey("keypad_3"),
-        "j": toKey("keypad_4"),
-        "k": toKey("keypad_5"),
-        "l": toKey("keypad_6"),
-        "u": toKey("keypad_7"),
-        "i": toKey("keypad_8"),
-        "o": toKey("keypad_9"),
-        "return_or_enter": toKey("keypad_0"),
+        g: [...terminateMode("SPECIAL"), toKey("g", "shift")],
+        i: toKey("keypad_8"),
+        j: toKey("keypad_4"),
+        k: toKey("keypad_5"),
+        l: toKey("keypad_6"),
+        m: toKey("keypad_1"),
+        o: toKey("keypad_9"),
+        u: toKey("keypad_7"),
+        [UJM.enter]: toKey("keypad_0"),
+        [UJM.半全]: terminateMode("SPECIAL"),
+        [UJM[":"]]: [...terminateMode("SPECIAL"), toKey("japanese_kana")],
       },
-      // 特殊
-      map(...JM[":"]).to(withTerminateMode("SPECIAL", toKey("japanese_kana"))),
-      map("g").to(withTerminateMode("SPECIAL", toKey("g", "shift"))),
-      // モード切り替え
-      map(...JM.無変換).to(terminateMode("SPECIAL")),
     ]),
 
   // ; combination
   layer(";").leaderMode().manipulators({
+    "/": toPaste(Temporal.Now.plainDateISO().toString().replaceAll("-", "/")),
+    ";": toJKey(";"),
     a: toJKey("^"),
     c: toJKeys("`", "`", "`"),
     d: toJKey("#"),
     e: toJKeys(" ", "=", " "),
-    r: toJKeys(" ", "=", "=", " "),
-    k: toJKeys("`", "`", "<-"),
     f: toJKey("$"),
-    h: toJKey("~"),
-    l: toJKey("_"),
-    o: toJKey("|"),
-    z: toJKey("!"),
     g: toJKey("&"),
-    m: toJKey('"'),
-    v: toJKey("'"),
-    p: toJKey("%"),
-    w: toKey("w", "control"), // ctrl+w
-    u: toJKeys('"', '"', "<-"),
-    s: toJKeys("(", ")", "<-"),
+    h: toJKey("~"),
     i: toJKeys("{", "}", "<-"),
-    ";": toJKey(";"),
+    k: toJKeys("`", "`", "<-"),
+    l: toJKey("_"),
+    m: toJKey('"'),
+    o: toJKey("|"),
+    p: toJKey("%"),
+    r: toJKeys(" ", "=", "=", " "),
+    s: toJKeys("(", ")", "<-"),
     t: toPaste(Temporal.Now.plainDateISO().toString()),
-    "/": toPaste(Temporal.Now.plainDateISO().toString().replaceAll("-", "/")),
+    u: toJKeys('"', '"', "<-"),
+    v: toJKey("'"),
+    w: toKey("w", "control"), // ctrl+w
+    z: toJKey("!"),
   }),
 
   rule("default").manipulators([
-    map("escape").to([toKey("escape"), toKey("japanese_eisuu")]),
-    map(...JM.無変換).to(
-      startMode("NORMAL"),
-    ),
+    withCondition(App.not("Ghostty"))([
+      withModifier("control")({
+        a: toKey("a", "command"),
+        f: toKey("f", "command"),
+        l: toKey("l", "command"),
+        t: toKey("t", "command"),
+      }),
+    ]),
 
-    // Control
+    {
+      [UJM.ESC]: toJKeys("ESC", "英数"),
+      [UJM.半全]: startMode("NORMAL"),
+    },
+
     withModifier("control")([
       {
         "8": toJKey("{"),
         "9": toJKey("}"),
+        q: toKey("q", "command"),
       },
-      map("a").to("a", "command").condition(App.not("Ghostty")),
-      map("t").to("t", "command").condition(App.not("Ghostty")),
-      map("l").to("l", "command").condition(App.not("Ghostty")),
-      map("f").to("f", "command").condition(App.not("Ghostty")),
-      map("q").to("q", "command"),
     ]),
 
-    // Command
     withModifier("command")({
-      "l": toKey("tab", "control"),
-      "h": toKey("tab", ["control", "shift"]),
       "2": toJKeys("#", "#", " "),
       "3": toJKeys("#", "#", "#", " "),
       "4": toJKeys("#", "#", "#", "#", " "),
       "5": toJKeys("#", "#", "#", "#", "#", " "),
+      h: toJKeyWith("tab", ["control", "shift"]),
+      l: toJKeyWith("tab", "control"),
     }),
   ]),
 ]);
