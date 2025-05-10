8f2f1809689e2265afd39b603d6fd86395cf303c
tadashi-aikawa <syou.maman@gmail.com>
2025-05-10 13:01:31 +0900

    feat(raycast): png-to-webpコマンドを追加

---
+++ raycast/png-to-webp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title png to webp
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon https://publish-01.obsidian.md/access/35d05cd1bf5cc500e11cc8ba57daaf88/favicon-64.png
+
+../mnt/toki/toki.sh webp
