d865c409454236d7413f4e0c8e2c0d2621182090
tadashi-aikawa <syou.maman@gmail.com>
2025-05-11 16:34:02 +0900

    refactor(karabiner): Finderに関するコメントアウト削除

--- karabiner/modes/normal.ts
+++ karabiner/modes/normal.ts
@@ -29,16 +29,6 @@ export const normalModeDefinitions = layer(UNUSED_KEY, "NORMAL")
         q: toKey("w", ["option", "shift"]),
       },
     ]),
-    // WARNING: Raycastと干渉するので一旦コメントアウト
-    // withCondition(App.is("Finder"))([
-    //   withModifier("command")({
-    //     e: toJKeyWith("]", "command"),
-    //   }),
-    //   {
-    //     o: toJKeyWith("bs", "command"),
-    //     e: toJKeyWith("[", "command"),
-    //   },
-    // ]),
     withCondition(App.is("Ghostty"))([
       withModifier("control")(likeCtrlCommands),
       withModifier("shift")({
