a7db1cc39150a66f7920c6ebee63cb3bb2458988
tadashi-aikawa <syou.maman@gmail.com>
2025-05-19 00:01:34 +0900

    style(vscode): FindItFasterの表示領域を最大化

--- mnt/fzf/fzf.sh
+++ mnt/fzf/fzf.sh
@@ -1,2 +1,5 @@
 export FZF_DEFAULT_OPTS="--reverse --border --height 50%"
-
+# FindItFasterを実行するために開かれたターミナル起動時は異なる設定にする
+if [[ $FIND_IT_FASTER_ACTIVE -eq 1 ]]; then
+  FZF_DEFAULT_OPTS="--reverse --border --height 90%"
+fi
