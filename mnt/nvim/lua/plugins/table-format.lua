2ff679e740a7a65c6146d0ce5cf58d1c90db0b2a
tadashi-aikawa <syou.maman@gmail.com>
2025-06-29 17:03:39 +0900

    feat(neovim): table-format Insert modeでも実行できるようにする

--- mnt/nvim/lua/plugins/table-format.lua
+++ mnt/nvim/lua/plugins/table-format.lua
@@ -1,7 +1,7 @@
 return {
   dir = "~/git/github.com/tadashi-aikawa/table-format.nvim",
   keys = {
-    { "<D-;>", "<Cmd>:TableFormat<CR>" },
+    { "<D-;>", "<Cmd>:TableFormat<CR>", mode = { "n", "i" } },
   },
   opts = {},
 }
