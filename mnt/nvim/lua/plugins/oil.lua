633a2038c1253cef1f68afc91833e7691edf4d9e
tadashi-aikawa <syou.maman@gmail.com>
2025-07-31 22:33:52 +0900

    feat(neovim): oil.nvimのキーマップを明示的に指定 (カレントディレクトリ移動を ~ にして _ を割り当てない)

--- mnt/nvim/lua/plugins/oil.lua
+++ mnt/nvim/lua/plugins/oil.lua
@@ -6,9 +6,20 @@ return {
   cmd = "Oil",
   opts = {
     skip_confirm_for_simple_edits = true,
+    use_default_keymaps = false,
     keymaps = {
-      ["<C-s>"] = "actions.select_split",
-      ["<C-CR>"] = "actions.select_vsplit",
+      ["<CR>"] = "actions.select",
+      ["<C-CR>"] = { "actions.select", opts = { vertical = true } },
+      ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-t>"] = { "actions.select", opts = { tab = true } },
+      ["<C-p>"] = "actions.preview",
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = { "actions.parent", mode = "n" },
+      ["~"] = { "actions.open_cwd", mode = "n" },
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
       ["gy"] = "actions.yank_entry",
       ["gR"] = {
         callback = function()
