39198c0a7bc6365ccf6b6ce35cd176ba01f32aa9
tadashi-aikawa <syou.maman@gmail.com>
2025-08-28 08:49:23 +0900

    feat(yazi): ~/Downloads への移動ショートカットを追加

--- mnt/yazi/init.lua
+++ mnt/yazi/init.lua
@@ -6,7 +6,7 @@ require("bunny"):setup({
 	hops = {
 		{ key = "/", path = "/" },
 		{ key = "t", path = "/tmp" },
-		{ key = "d", path = "~/Desktop", desc = "Desktop" },
+		{ key = "d", path = "~/Downloads", desc = "Downloads" },
 		{ key = "D", path = "~/Documents", desc = "Documents" },
 		{ key = "c", path = "~/.config", desc = "Config files" },
 		{ key = "i", path = "~/Library/Mobile Documents/com~apple~CloudDocs/image", desc = "iCloud/image" },
