88a188e78b27756b26cbb5ba4a7c6997ef51416f
tadashi-aikawa <syou.maman@gmail.com>
2025-08-29 08:28:20 +0900

    docs(yazi): bunny-privateの説明を追加

--- mnt/yazi/init.lua
+++ mnt/yazi/init.lua
@@ -2,4 +2,20 @@ require("git"):setup()
 require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
+
+-- `plugins/bunny-private.yazi/main.lua` に設定する
+--
+-- ```lua
+-- local function setup()
+-- 	require("bunny"):setup({
+-- 		hops = {
+-- 			{ key = "/", path = "/" },
+-- 			{ key = "d", path = "~/Downloads", desc = "Downloads" },
+-- 		},
+-- 	})
+-- end
+--
+-- return { setup = setup }
+-- ```
+--
 require("bunny-private"):setup()
