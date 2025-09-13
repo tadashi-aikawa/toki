762f8a4322bc5a382024f53b0251caef92995875
tadashi-aikawa <syou.maman@gmail.com>
2025-09-14 01:07:37 +0900

    feat(yazi): folder-rulesを追加 (Pictures/screenshotsディレクトリのソート順変更)

--- mnt/yazi/plugins/folder-rules.yazi/main.lua
+++ mnt/yazi/plugins/folder-rules.yazi/main.lua
@@ -1,7 +1,9 @@
 local function setup()
+	local home = os.getenv("HOME") or ""
+
 	ps.sub("cd", function()
 		local cwd = cx.active.current.cwd
-		if cwd:ends_with("Downloads") then
+		if cwd:ends_with("Downloads") or cwd:starts_with(home .. "/Documents/Pictures/screenshots/") then
 			ya.emit("sort", { "mtime", reverse = true, dir_first = false })
 		else
 			ya.emit("sort", { "alphabetical", reverse = false, dir_first = true })
