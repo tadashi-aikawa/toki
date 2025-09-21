837dc589b050e37f03f6d1a0cd817fd587d5465e
tadashi-aikawa <syou.maman@gmail.com>
2025-09-21 22:17:15 +0900

    feat(fzf/zoxide/yazi): fzfウィンドウのマージンとプレビュー比率を修正

--- mnt/fzf/fzf.sh
+++ mnt/fzf/fzf.sh
@@ -2,5 +2,5 @@ export FZF_DEFAULT_OPTS="
   --reverse
   --style=full:rounded
   --height 45%
-  --margin 0,15
+  --margin 0,5%
 "
--- mnt/yazi/yazi.sh
+++ mnt/yazi/yazi.sh
@@ -18,8 +18,8 @@ export YAZI_ZOXIDE_OPTS="
   --reverse
   --style=full:rounded
   --height 75%
-  --margin 0,15
-  --preview-window=down,20,wrap
+  --margin 0,5%
+  --preview-window=down,50%,wrap
   --preview 'eza -l --icons --sort modified -r --color=always {2..}'
   --no-sort
   --exact
