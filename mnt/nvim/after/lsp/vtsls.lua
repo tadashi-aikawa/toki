6d47f360bfe74343a717bbe11ae5e20e81bfcc5c
tadashi-aikawa <syou.maman@gmail.com>
2025-09-23 14:19:54 +0900

    fix(lsp): vtslsがVueプロジェクトでも起動しない

--- mnt/nvim/after/lsp/vtsls.lua
+++ mnt/nvim/after/lsp/vtsls.lua
@@ -1,5 +1,3 @@
-local util = require("lspconfig.util")
-
 local vue_language_server_path = os.getenv("HOME")
   .. "/.local/share/mise/installs/npm-vue-language-server/latest/lib/node_modules/@vue/language-server"
 local vue_plugin = {
@@ -11,7 +9,11 @@ local vue_plugin = {
 
 return {
   workspace_required = true,
-  root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
+  root_dir = function(bufnr, on_dir)
+    local root_markers = { "package.json", "tsconfig.json", "jsconfig.json" }
+    local project_root = vim.fs.root(bufnr, root_markers)
+    on_dir(project_root)
+  end,
   settings = {
     vtsls = {
       tsserver = {
