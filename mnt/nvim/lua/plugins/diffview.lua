0bcd0441c29bb5f0835afb2d608d5c89f61ab2e0
tadashi-aikawa <syou.maman@gmail.com>
2025-10-21 23:15:36 +0900

    feat(neovim): Diffview.nvimで折りたたまずに表示する

--- mnt/nvim/lua/plugins/diffview.lua
+++ mnt/nvim/lua/plugins/diffview.lua
@@ -1,4 +1,11 @@
 return {
   "sindrets/diffview.nvim",
   cmd = "DiffviewOpen",
+  opts = {
+    hooks = {
+      diff_buf_win_enter = function()
+        vim.opt_local.foldenable = false
+      end,
+    },
+  },
 }
