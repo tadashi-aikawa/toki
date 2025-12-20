af5cbbda0bfd13269510ea9f737b007c29cafb01
tadashi-aikawa <syou.maman@gmail.com>
2025-12-20 10:51:34 +0900

    feat(folder-rules): DownloadsフォルダとPicturesフォルダの両方でmtimeソートを適用

--- mnt/yazi/plugins/folder-rules.yazi/main.lua
+++ mnt/yazi/plugins/folder-rules.yazi/main.lua
@@ -3,7 +3,7 @@ local function setup()
 
 	ps.sub("cd", function()
 		local cwd = cx.active.current.cwd
-		if cwd:ends_with("Downloads") or cwd:starts_with(home .. "/Documents/Pictures/screenshots/") then
+		if cwd:ends_with("Downloads") or cwd:starts_with(home .. "/Documents/Pictures/") then
 			ya.emit("sort", { "mtime", reverse = true, dir_first = false })
 		else
 			ya.emit("sort", { "alphabetical", reverse = false, dir_first = true })
