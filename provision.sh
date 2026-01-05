c7df5b9629eaada614e44ee2c8d232a853f03208
tadashi-aikawa <syou.maman@gmail.com>
2026-01-05 18:26:15 +0900

    chore(yazi): v26.1.4で動かない機能があるので一時的に無効化

--- provision.sh
+++ provision.sh
@@ -421,7 +421,8 @@ brew install convmv
 brew install clipboard
 
 # yazi
-brew install yazi font-symbols-only-nerd-font
+# TODO: yazi v26.1.4 が動くようになったら解除
+# brew install yazi font-symbols-only-nerd-font
 ln -snf "$MNT"/yazi/yazi.sh ~/.yazi.sh
 mkdir -p ~/.config/yazi
 ln -snf "$MNT"/yazi/yazi.toml ~/.config/yazi/yazi.toml
