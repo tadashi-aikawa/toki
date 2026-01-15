7293ac0bd1df94253e30a684380fb69c6fe41ac7
tadashi-aikawa <syou.maman@gmail.com>
2026-01-15 18:55:37 +0900

    feat(nvim): screenkey.nvimを自動起動可能にする設定を追加

--- mnt/nvim/lua/plugins/screenkey.lua
+++ mnt/nvim/lua/plugins/screenkey.lua
@@ -1,6 +1,7 @@
 return {
   "NStefan002/screenkey.nvim",
-  cmd = "Screenkey",
+  -- INFO: 自動起動したい場合
+  -- lazy = false,
   opts = {
     compress_after = 5,
     group_mappings = true,
@@ -16,4 +17,9 @@ return {
     },
     clear_after = 5,
   },
+  -- INFO: 自動起動したい場合
+  -- config = function(_, opts)
+  --   require("screenkey").setup(opts)
+  --   require("screenkey").toggle()
+  -- end,
 }
