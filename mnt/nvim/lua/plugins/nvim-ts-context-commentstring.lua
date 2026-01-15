c8f306a0d758cc99471b66679db5049d9fc1cffb
tadashi-aikawa <syou.maman@gmail.com>
2026-01-15 21:07:09 +0900

    feat(nvim): nvim-ts-context-commentstringのLspAttachイベントでの遅延読み込みを追加

--- mnt/nvim/lua/plugins/nvim-ts-context-commentstring.lua
+++ mnt/nvim/lua/plugins/nvim-ts-context-commentstring.lua
@@ -1,5 +1,6 @@
 return {
   "JoosepAlviste/nvim-ts-context-commentstring",
+  event = { "LspAttach" },
   config = function()
     require("ts_context_commentstring").setup({
       enable_autocmd = false,
