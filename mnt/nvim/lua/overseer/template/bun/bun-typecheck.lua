b52fedbd8c7083e689895dfc38b70e2be1de82a0
tadashi-aikawa <syou.maman@gmail.com>
2026-02-11 16:27:13 +0900

    feat(overseer): Bunのtypecheckタスクテンプレートを追加

---
+++ mnt/nvim/lua/overseer/template/bun/bun-typecheck.lua
@@ -0,0 +1,28 @@
+---@type overseer.TemplateDefinition
+return {
+  name = "🎭bun typecheck",
+  builder = function()
+    return {
+      name = "bun typecheck",
+      strategy = {
+        "orchestrator",
+        tasks = {
+          { "🦉bun typecheck" },
+        },
+      },
+      components = {
+        { "open_output", on_start = "never" },
+        { "on_complete_notify", on_change = true, statuses = {} },
+        {
+          "on_children_status_sync",
+          task_names = { "bun typecheck" },
+        },
+        {
+          "on_complete_trouble_close_if_clean",
+          task_names = { "bun typecheck" },
+        },
+        "default",
+      },
+    }
+  end,
+}
