a2e2b30e1c3ef3153af679a6618b268072b7bed3
tadashi-aikawa <syou.maman@gmail.com>
2026-03-21 18:16:33 +0900

    feat(otm): ランダム10桁をopenssl rand生成に変更

--- mnt/otm/otm.sh
+++ mnt/otm/otm.sh
@@ -74,7 +74,7 @@ cmd_create() {
     ;;
   esac
   local id
-  id=$(uuidgen | tr '[:upper:]' '[:lower:]' | cut -c1-10)
+  id=$(openssl rand -hex 5)
   local datetime
   datetime=$(date '+%Y-%m-%dT%H:%M')
   local filepath="${TASKS_DIR}/${name}.md"
