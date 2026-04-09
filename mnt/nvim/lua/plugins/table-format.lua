301657cc82a389fd3c86293707dfe9843e3c8618
tadashi-aikawa <syou.maman@gmail.com>
2026-04-09 19:11:45 +0900

    feat(nvim): テーブルフォーマットのキーマップを変更 (cmuxでも使えるように)

--- mnt/nvim/lua/plugins/table-format.lua
+++ mnt/nvim/lua/plugins/table-format.lua
@@ -2,7 +2,7 @@ return {
   dir = "~/git/github.com/tadashi-aikawa/table-format.nvim",
   cond = true,
   keys = {
-    { "<D-;>", "<Cmd>:TableFormat<CR>", mode = { "n", "i" } },
+    { "<D-:>", "<Cmd>:TableFormat<CR>", mode = { "n", "i" } },
   },
   opts = {},
 }
