bef5325dd0e81df82c58b9967c22ec9c04b8014e
tadashi-aikawa <syou.maman@gmail.com>
2026-06-02 07:40:21 +0900

    feat(cmux): homebrewのtapを削除

--- provision.sh
+++ provision.sh
@@ -158,7 +158,6 @@ ln -snf "$MNT"/ghostty/config "$GHOSTTY_CONFIG_DIR"/config
 ln -snf "$MNT"/ghostty/shaders "$GHOSTTY_CONFIG_DIR"/shaders
 
 # cmux
-brew tap manaflow-ai/cmux
 brew install --cask cmux
 ln -snf "$MNT"/cmux/cmux.sh ~/.cmux.sh
 ensure_zshrc "source ~/.cmux.sh"
