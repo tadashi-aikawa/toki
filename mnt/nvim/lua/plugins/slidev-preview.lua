9dcf094afe08c71a22c35897e53d8aaf33c03737
tadashi-aikawa <syou.maman@gmail.com>
2026-06-20 13:14:02 +0900

    feat(nvim): slidev-preview.nvimにスライドのクリック数を増減させるコマンドのキーマップを追加

--- mnt/nvim/lua/plugins/slidev-preview.lua
+++ mnt/nvim/lua/plugins/slidev-preview.lua
@@ -3,4 +3,8 @@ return {
   dir = "~/git/github.com/tadashi-aikawa/slidev-preview.nvim",
   cmd = { "SlidevPreviewStart", "SlidevPreviewStartAndOpen" },
   opts = {},
+  keys = {
+    { ",sj", "<cmd>SlidevPreviewClicksIncrement<cr>", desc = "Slidev clicks +1" },
+    { ",sk", "<cmd>SlidevPreviewClicksDecrement<cr>", desc = "Slidev clicks -1" },
+  },
 }
